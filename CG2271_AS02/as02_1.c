2778b71f70b810bf69ad05e541af1ee8cb769c66
Ksheerabthi Nathan <vksheerabthi@gmail.com>
2026-02-13 14:22:21 +0800

    Section 1 code

--- CG2271_AS02/as02_1.c
+++ CG2271_AS02/as02_1.c
@@ -29,17 +29,79 @@ typedef enum tl {
 } TLED;
 
 //TODO
-void setMCGIRClk()
+void setMCGIRCLK() {
+	//clear CLKS
+	MCG->C1 &= ~MCG_C1_CLKS_MASK;
 
-void initTimer()
+	//set IRCLKEN to enable LIRC
+	MCG->C1 |= ((MCG_C1_CLKS(0b01)| MCG_C1_IRCLKEN_MASK));
 
-void startTimer()
+	//choose the 2MHz Clock
+	MCG->C2 &= ~MCG_C2_IRCS_MASK;
 
-void stopTimer()
+	//set FRCDIV and LIRC_DIV2
+	MCG->SC &= ~MCG_SC_FCRDIV_MASK;
+	MCG->SC |= MCG_SC_FCRDIV(0b0);
+	MCG->MC &= ~MCG_MC_LIRC_DIV2_MASK;
+	MCG->MC |= MCG_MC_LIRC_DIV2(0b001);
+}
+
+void initTimer() {
+	NVIC_DisableIRQ(TPM0_IRQn);
+    setMCGIRClk();
+
+    //Turn on clock gating for TPM0
+    SIM->SCGC6|=SIM_SCGC6_TPM0_MASK;
+    SIM->SOPT2&=~SIM_SOPT2_TPMSRC_MASK;
+
+    //Clear TPM clock source and select MCGIRCLK
+    SIM->SOPT2|=SIM_SOPT2_TPMSRC(0b11);
+
+    //Turn off TPM0 and clear Prescale counter
+    TPM0->SC&=~(TPM_SC_CMOD_MASK | TPM_SC_PS_MASK);
+
+    //Initialize counter to 0
+    TPM0->CNT=0;
+
+    //Set Prescale Counter
+    TPM0->SC|= ((TPM_SC_TOIE_MASK) | TPM_SC_PS(0b011));
+
+    //Initialize modulo
+    TPM0->MOD = 62500;
+
+    //Set priority to highest and enable IRQ
+    NVIC_SetPriority(TPM0_IRQn, 0);
+
+    NVIC_EnableIRQ(TPM0_IRQn);
+    //setMCGIRClk(); //uncomment this after question 2
+}
+
+void startTimer() {
+    TPM0->SC |= TPM_SC_CMOD(0b1);
+}
+
+void stopTimer() {
+    TPM0->SC &= ~TPM_SC_CMOD_MASK;
+}
 
 //count needs to be volatile.
 volatile int count = 0;
-void TPM0_IRQHandler() 
+void TPM0_IRQHandler() {
+    //clear pending IRQ
+	NVIC_ClearPendingIRQ(TPM0_IRQr);
+
+	//Check if TOF is set
+	if(TPM0->STATUS & TPM_STATUS_TOF_MASK) {
+		// IF TOF is set, 1. count = (count + 1) % 6
+		count = (count + 1) % 6;
+
+		// 2. reset counter
+		TPM0->CNT = 0;
+
+		// 3. Clear TOF bit
+		TPM0->STATUS |= TPM_STATUS_TOF_MASK;
+	}
+}
 
 void initGPIO()
 
